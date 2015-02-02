534035e75ae1e92afe6c98e51480722500167b9a
Simon Asika <asika32764@gmail.com>
2015-02-02 20:35:31 +0800

    Fix form.js bug

--- media/js/form.js
+++ media/js/form.js
@@ -1,50 +1,61 @@
-
-$(function() {
-	$.urlParam = function(name){
+$(function()
+{
+	$.urlParam = function(name)
+	{
 		var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-		if(results) {
+
+		if (results)
+		{
 			return results[1] || 0;
 		}
+
 		return '';
 	};
 
-	$('#api-public-ticket form input').each(function(){
+	$('form#contact-us input').each(function()
+	{
 		var v = $.urlParam($(this).attr('name'));
-		if(v) {
+
+		if (v)
+		{
 			$(this).val(decodeURIComponent(v.replace(/\+/g, ' ')));
 		}
 	});
 
 	var err = $.urlParam('error_msg');
-	if(err) {
+
+	if (err)
+	{
 		$('<div class="error uk-alert uk-alert-warning">').html(decodeURIComponent(err.replace(/\+/g, ' ')))
 			.prependTo('#messages');
 	}
 });
 
-
-function urldecode(url) {
+function urldecode(url)
+{
 	return decodeURIComponent(url.replace(/\+/g, ' '));
 }
 
-
-$.urlParam = function(name){
+$.urlParam = function(name)
+{
 	var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
-	if(results) {
+
+	if (results)
+	{
 		return results[1] || 0;
 	}
 	return '';
 };
 
-
-$(function() {
-	$('#reg input').each(function(){
+$(function()
+{
+	$('#reg input').each(function()
+	{
 		var v = $.urlParam($(this).attr('name'));
-		if(v) {
+
+		if (v)
+		{
 			$(this).val(urldecode(v));
 		}
 	});
 });
-
-
-
